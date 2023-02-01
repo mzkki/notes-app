1f76c2cfb2f8a19c34fc2a328a2cb884db525ce5
mzkki <08harismu@gmail.com>
2023-02-01 14:00:41 +0800

    fix button form and styling font family

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
+import './css/style.css'
 
 function App() {
   return (
--- src/components/form/FormButton.js
+++ src/components/form/FormButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { useLocation } from "react-router";
 
 function FormButton() {
+  const currentPath = useLocation()
+
   return (
-    <Button variant="success" className="w-100 mt-4 ">Login</Button>
+    <Button variant="success" className="w-100 mt-4 ">{currentPath.pathname === '/register' ? 'Register' : 'Login'}</Button>
   )
 }
 
