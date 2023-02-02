5649265cb6bb7eb2b1af1cd4c1db231845ff05f2
mzkki <08harismu@gmail.com>
2023-02-02 15:01:25 +0800

    add proptypes

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 function useInput(defaultValue) {
   const [value, setValue] = React.useState(defaultValue)
   const handleValueChange = (event) => setValue(event.target.value)
   return [value, handleValueChange]
+}
+
+useInput.propTypes = {
+  defaultValue: PropTypes.string
 }
 
 export default useInput;
