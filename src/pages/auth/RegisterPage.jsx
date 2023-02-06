683c57ddac2bad180a97a69f780a8a05faf1e2cd
mzkki <08harismu@gmail.com>
2023-02-06 13:03:08 +0800

    fix card error on register

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-import { Card, Row } from "react-bootstrap";
-import RegisterForm from "../../components/form/RegisterForm";
-import FormButton from "../../components/form/FormButton";
-import { Link } from "react-router-dom";
-
+import React from 'react';
+import { Card, Row } from 'react-bootstrap';
+import RegisterForm from '../../components/form/RegisterForm';
+import FormButton from '../../components/form/FormButton';
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
   return (
     <Row className="d-flex justify-content-center">
-      <Card Card style={{ width: '30rem' }
-      } className="shadow border-0 p-4 mt-5" >
+      <Card style={{ width: '30rem' }} className="shadow border-0 p-4 mt-5">
         <Card.Body className="my-5">
           <Card.Title className="mb-4 text-center">Register Page</Card.Title>
           <RegisterForm />
           <FormButton />
-          <p>Sudah punya akun ? <Link to={'/'} className="text-decoration-none">Login</Link> </p>
+          <p>
+            Sudah punya akun ?{' '}
+            <Link to={'/'} className="text-decoration-none">
+              Login
+            </Link>{' '}
+          </p>
         </Card.Body>
-      </Card >
-    </Row >
-  )
+      </Card>
+    </Row>
+  );
 }
 
 export default RegisterPage;
